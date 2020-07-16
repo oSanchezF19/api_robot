b0044030ef4070a6f0739963a7b828e14948a4e2
Oscar Sanchez <oscar@microbit.com>
2020-07-15 22:27:35 -0500

    version2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -11,67 +11,126 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-class Post(db.Model):
-    id =db.Column(db.Integer, primary_key=True)
+tags = db.Table('tags',
+    db.Column('temp_id', db.Integer, db.ForeignKey('temp.temp_id')),
+    db.Column('page_id', db.Integer, db.ForeignKey('hum.hum_id'))
+)
+
+class Temp(db.Model):
+    temp_id =db.Column(db.Integer, primary_key=True)
     temp = db.Column(db.Float)
+    date = db.Column(db.String(100))
+
+class Hum(db.Model):
+    hum_id =db.Column(db.Integer, primary_key=True)
     hum = db.Column(db.Float)
     date = db.Column(db.String(100))
 
 class PostSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'temp', 'hum', 'date')
+        fields = ('temp_id', 'hum_id', 'temp', 'hum', 'date')
 
 post_schema = PostSchema()
 posts_schema = PostSchema(many = True)
 
 
-class PostsResource(Resource):
+class temperatura(Resource):
     def get(self):
-            return posts_schema.dump(Post.query.all())  
-    
+            return posts_schema.dump(Temp.query.all()) 
+
     def post(self):
-        try:
-            data = request.json
-            
-            if 'hum' in data:
-                post = Post(hum=data['hum'], date=datetime.now())
-            if 'temp' in data:
-                post = Post(temp=data['temp'], date=datetime.now())
-            if 'temp' and 'hum' in data:
-                post = Post(temp=data['temp'],hum=data['hum'], date=datetime.now())
+        data = request.json
+        try:     
+            post = Temp(temp=data['temp'], date=datetime.now())
             db.session.add(post)
             db.session.commit()
             return post_schema.dump(post)
-        except:
-            return jsonify({"mensaje:":"error en la solicitud"}), 400
 
-class PostResource(Resource):
+        except:
+            response = jsonify({"mensaje:":"error en la solicitud"})
+            response.status_code = 400
+            return response
+
+          
+class tempid(Resource):
     def get(self, pk):
-        return post_schema.dump(Post.query.get_or_404(pk))
+        return post_schema.dump(Temp.query.get_or_404(pk))
 
     def put(self, pk):
         data = request.json
-        post = Post.query.get_or_404(pk)
+        post = Temp.query.get_or_404(pk)
+        try:
+            
+            if 'temp' in data:
+                post.temp = data['temp']
+            if 'date' in data:
+                post.date = data['date']
 
-        if 'temp' in data:
-            post.temp = data['temp']
-        if 'hum' in data:
-            post.hum = data['hum']
-        if 'date' in data:
-            post.date = data['date']
+            db.session.commit()
+            return post_schema.dump(post)
 
-        db.session.commit()
-        return post_schema.dump(post)
+        except:
+            response = jsonify({"mensaje:":"error en la solicitud"})
+            response.status_code = 400
+            return response
 
     def delete(self, pk):
-        post = Post.query.get_or_404(pk)
+        post = Temp.query.get_or_404(pk)
         db.session.delete(post)
         db.session.commit()
 
         return '', 204
 
-api.add_resource(PostResource, '/post/<int:pk>')
-api.add_resource(PostsResource, '/posts')
+class humedad(Resource):
+    def get(self):
+            return posts_schema.dump(Hum.query.all()) 
+
+    def post(self):
+        data = request.json
+        try:
+            post = Hum(hum=data['hum'], date=datetime.now())
+            db.session.add(post)
+            db.session.commit()
+            return post_schema.dump(post)
+        except:
+            response = jsonify({"mensaje:":"error en la solicitud"})
+            response.status_code = 400
+            return response
+          
+class humid(Resource):
+    def get(self, pk):
+        return post_schema.dump(Hum.query.get_or_404(pk))
+
+    def put(self, pk):
+        data = request.json
+        post = Hum.query.get_or_404(pk)
+        try:
+            if 'hum' in data:
+                post.hum = data['hum']
+            if 'date' in data:
+                post.date = data['date']
+
+            db.session.commit()
+            return post_schema.dump(post)
+        except:
+            response = jsonify({"mensaje:":"error en la solicitud"})
+            response.status_code = 400
+            return response
+
+    def delete(self, pk):
+        post = Hum.query.get_or_404(pk)
+        db.session.delete(post)
+        db.session.commit()
+
+        return '', 204
+
+api.add_resource(temperatura, '/temp')
+api.add_resource(tempid, '/temp/<int:pk>')
+api.add_resource(humedad, '/hum')
+api.add_resource(humid, '/hum/<int:pk>')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug= True)
